3dba46c244a936b9e42c982ae6bca7e4385f1a0a
mleroux2017 <ml.maroux@gmail.com>
2018-05-30 16:00:59 +0200

    ajout DAL

---
+++ KermesseDAL/WKContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebKermesse.Models;
+using KermesseBO;
+using System.Data.Entity;
+
+namespace KermesseDAL
+{
+   
+    public class WKContext : IdentityDbContext<ApplicationUser>
+    {
+        
+            public WKContext()
+                : base("DefaultConnection", throwIfV1Schema: false)
+            {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<WKContext, MLV.DAL.Migrations.Configuration>());
+        }
+
+            public static WKContext Create()
+            {
+                return new WKContext();
+        }
+        
+       
+
+
+
+}
+    }
+}
