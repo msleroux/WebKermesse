8b71e3cf91dcced8e983e129f34931f6831c76ef
mleroux2017 <ml.maroux@gmail.com>
2018-05-30 14:56:31 +0200

    init

---
+++ WebKermesse/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebKermesse.Startup))]
+namespace WebKermesse
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
